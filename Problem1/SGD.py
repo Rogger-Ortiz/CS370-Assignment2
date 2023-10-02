25776199d3b56f9e46b695e31702756e88ef2b54
Rogger-Ortiz <contactme@rjortiz.com>
2023-10-02 12:46:43 -0400

    P1

--- Problem1/SGD.py
+++ Problem1/SGD.py
@@ -1,25 +1,19 @@
-# All Imports
-
 import pandas as pd
 import sklearn
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 from pathlib import Path
 import numpy as np
 from sklearn.preprocessing import add_dummy_feature
 from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import SGDRegressor
+import matplotlib as mpl
 
-# Setup
+dataset_url = "https://raw.githubusercontent.com/plotly/datasets/master/imports-85.csv"
+df = pd.read_csv(dataset_url)
+np.random.seed(42)
+pd.set_option('display.max_columns', None)
 
-np.random.seed(69)
-plt.rc('font', size=14)
-plt.rc('axes', labelsize=14, titlesize=14)
-plt.rc('legend', fontsize=14)
-plt.rc('xtick', labelsize=10)
-plt.rc('ytick', labelsize=10)
-
-df = pd.read_csv("imports-85.csv")
+target_variable_column = df[['city-mpg']]
+feature_columns = df[['curb-weight', 'engine-size']]
 
 IMAGES_PATH = Path() / "images" / "training_linear_models"
 IMAGES_PATH.mkdir(parents=True, exist_ok=True)
@@ -31,137 +25,77 @@ def save_fig(fig_id, tight_layout=True, fig_extension="png", resolution=300):
         plt.tight_layout()
     plt.savefig(path, format=fig_extension, dpi=resolution)
 
-# Linear Regression
+### Linear Regression ###
 
 
-m = 205
-X = df['engine-size'].values
-y = df['curb-weight'].values
+m = len(df)
+X = target_variable_column.values
+y = feature_columns.values
 
 plt.figure(figsize=(6, 4))
 plt.plot(X, y, "b.")
-plt.xlabel("$Engine Size$")
-plt.ylabel("$Curb Weight$", rotation=0)
+plt.xlabel("$City MPG$")
+plt.ylabel("$CW and ES$", rotation=0)
 plt.grid()
 save_fig("generated_data_plot")
 plt.show()
 
-###########################################
-
-X_b = add_dummy_feature(df['engine-size'])
+X_b = add_dummy_feature(X)  # add x0 = 1 to each instance
 theta_best = np.linalg.inv(X_b.T @ X_b) @ X_b.T @ y
 
-X_new = np.array([[0], [2]])
+X_new = np.array([[0], [50]])
 X_new_b = add_dummy_feature(X_new)
 y_predict = X_new_b @ theta_best
+y_predict
 
-plt.figure(figsize=(6, 4))
+plt.figure(figsize=(6, 4))  # extra code – not needed, just formatting
 plt.plot(X_new, y_predict, "r-", label="Predictions")
 plt.plot(X, y, "b.")
-
-plt.xlabel("$x_1$")
-plt.ylabel("$y", rotation=0)
-plt.axis([0, 2, 0, 15])
+plt.xlabel("$City MPG$")
+plt.ylabel("$CW and ES$", rotation=0)
 plt.grid()
-plt.legend(loc="upper left")
 save_fig("linear_model_predictions_plot")
 plt.show()
 
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
+lin_reg.intercept_, lin_reg.coef_
 lin_reg.predict(X_new)
 theta_best_svd, residuals, rank, s = np.linalg.lstsq(X_b, y, rcond=1e-6)
 np.linalg.pinv(X_b) @ y
 
-# GRADIENT DESCENT
-# - Batch Gradient Descent
-
-eta = 0.1
-n_epochs = 1000
-m = len(X_b)
-
-theta = np.random.randn(2, 1)
-for epoch in range(n_epochs):
-    gradients = 2 / m * X_b.T @ (X_b @ theta - y)
-    theta = theta - eta * gradients
-
-
-def plot_gradient_descent(theta, eta):
-    m = len(X_b)
-    plt.plot(X, y, "b.")
-    n_epochs = 1000
-    n_shown = 20
-    theta_path = []
-    for epoch in range(n_epochs):
-        if epoch < n_shown:
-            y_predict = X_new_b @ theta
-            color = mpl.colors.rgb2hex(plt.cm.OrRd(epoch / n_shown + 0.15))
-            plt.plot(X_new, y_predict, linestyle="solid", color=color)
-        gradients = 2 / m * X_b.T @ (X_b @ theta - y)
-        theta = theta - eta * gradients
-        theta_path.append(theta)
-    plt.xlabel("$x_1$")
-    plt.axis([0, 2, 0, 15])
-    plt.grid()
-    plt.title(fr"$\eta = {eta}$")
-    return theta_path
-
-
-theta = np.random.randn(2, 1)
-
-plt.figure(figsize=(10, 4))
-plt.subplot(131)
-plot_gradient_descent(theta, eta=0.02)
-plt.ylabel("$y$", rotation=0)
-plt.subplot(132)
-theta_path_bgd = plot_gradient_descent(theta, eta=0.1)
-plt.gca().axes.yaxis.set_ticklabels([])
-plt.subplot(133)
-plt.gca().axes.yaxis.set_ticklabels([])
-plot_gradient_descent(theta, eta=0.5)
-save_fig("gradient_descent_plot")
-plt.show()
-
-# - Stochastic Gradient Descent
-
 theta_path_sgd = []
 n_epochs = 50
-t0, t1 = 5, 50
+t0, t1 = 5, 50  # learning schedule hyperparameters
 
 
 def learning_schedule(t):
-    return t0 / (t+t1)
+    return t0 / (t + t1)
 
 
-theta = np.random.randn(2, 1)
+theta = np.random.randn(2, 1)  # random initialization
 
-n_shown = 20
-plt.figure(figsize=(6, 4))
-
+n_shown = 20  # extra code – just needed to generate the figure below
+plt.figure(figsize=(6, 4))  # extra code – not needed, just formatting
 for epoch in range(n_epochs):
     for iteration in range(m):
+        # extra code – these 4 lines are used to generate the figure
         if epoch == 0 and iteration < n_shown:
-            y_predict = X_new_b @ theta
-            color = mpl.colors.rgb2hex(
-                plt.cm.OrRd(iteration / n_shown + 0.15))
+            y_predict = X_new_b @ theta_best
+            color = mpl.colors.rgb2hex(plt.cm.OrRd(iteration / n_shown + 0.15))
             plt.plot(X_new, y_predict, color=color)
 
         random_index = np.random.randint(m)
         xi = X_b[random_index: random_index + 1]
         yi = y[random_index: random_index + 1]
-        gradients = 2*xi.T @ (xi @ theta - yi)
+        # for SGD, do not divide by m
+        gradients = 2 * xi.T @ (xi @ theta - yi)
         eta = learning_schedule(epoch * m + iteration)
         theta = theta - eta * gradients
+        # extra code – to generate the figure
         theta_path_sgd.append(theta)
 
+# extra code – this section beautifies and saves Figure 4–10
 plt.plot(X, y, "b.")
-plt.xlabel("$x_1$")
-plt.ylabel("$y$", rotation=0)
-plt.axis([0, 2, 0, 15])
 plt.grid()
-save_fig("sgd_plot")
 plt.show()
-
-sgd_reg = SGDRegressor(max_iter=1000, tol=1e-5, penalty=None,
-                       eta0=0.01, n_iter_no_change=100, random_state=42)
-sgd_reg.fit(X, y.ravel())
